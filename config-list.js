d0bef40c1fead79c9e7dbf1efeae8cb360a7182d
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-05-18 15:21:08 +0800

    西元2025年05月18日 (週日) 15時21分08秒 CST

---
+++ config-list.js
@@ -0,0 +1,34 @@
+const ItemFiltersPreset = require('./app/config/ItemFiltersPreset.js')
+const CONFIG = require('../config-json.js')
+
+let feedList = [
+  {
+    title: '神奇買家 買東西',
+    feedID: 'SurpriseBuyer',
+    homepageURL: 'https://www.youtube.com/channel/UCjL1ratS2reFUKJra9Ahd2w',
+    // itemFilters: ItemFiltersPreset.between3minTo30Min,
+  },
+  
+
+  // ====== 以上 15 個，滿了！！ ========
+]
+
+// 
+
+// --------------------------------------
+
+if (CONFIG.debug) {
+  feedList = [
+    {
+      title: 'IT咖啡馆',
+      feedID: 'it-coffee',
+      homepageURL: 'https://www.youtube.com/channel/UCXLV-KfDQAFUJ_as9H1Lfbw',
+      itemFilters: [
+        ItemFiltersPreset.between3minTo30Min,
+      ] // 霸道總裁攀岩開會、網紅天天搞小團體...《絕世網紅》劇情超浮誇？《 志祺今天不讀稿 》EP010｜志祺七七
+    },
+  ]
+}
+
+
+module.exports = feedList
